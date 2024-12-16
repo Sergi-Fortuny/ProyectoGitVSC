ca7e2c03f85f294cc867bbfae16c3d88898f1e2a
Sergi-Fortuny <serforalb@alu.edu.gva.es>
2024-12-16 22:20:16 +0100

    Commit tareas.java

---
+++ Tareas.java
@@ -0,0 +1,10 @@
+public class Tareas {
+    public void imprimirTareas() {
+        System.out.println("Lista de tareas:");
+        // Aquí iría la lista de tareas
+    }
+
+    public void agregarTarea(String tarea) {
+        System.out.println("Tarea agregada: " + tarea);
+    }
+}
