cd1f5b22eae2c4d6aa15f190d34f005c7549c7d6
Sergi-Fortuny <serforalb@alu.edu.gva.es>
2024-12-16 22:26:53 +0100

    Commit de  Saludo.java

---
+++ Saludo.java
@@ -0,0 +1,9 @@
+public class Saludo {
+    public void saludoInicial() {
+        System.out.println("¡Hola! Bienvenido al programa.");
+    }
+
+    public void despedida() {
+        System.out.println("Adiós. ¡Hasta la próxima!");
+    }
+}
